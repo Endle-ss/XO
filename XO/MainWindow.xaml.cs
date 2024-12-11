fb0ad670ece322939f0c01306c483b9004616fff
Koiru <141869441+Endle-ss@users.noreply.github.com>
2024-12-11 14:19:29 +0300

    Добавьте файлы проекта.

---
+++ XO/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+namespace XO
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayField playField = new PlayField();
+            playField.Show();
+            this.Close(); 
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
